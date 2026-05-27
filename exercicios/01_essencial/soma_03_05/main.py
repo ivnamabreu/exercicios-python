dd8b48c875933338994029f2fccc264b9b107301
Ívna Mascarenhas e Abreu <ivna.mabreu@gmail.com>
2026-05-27 20:54:29 +0000

    Add exercicios soma_03_05 e temperatura

--- exercicios/01_essencial/soma_03_05/main.py
+++ exercicios/01_essencial/soma_03_05/main.py
@@ -1,3 +1,14 @@
-def resposta(n):
-    pass
+def resposta(n = 54):
+    soma = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            soma += i
+    return(soma)
+
+
+
+#soma de todos os números menores que `n` que sejam múltiplos de 3 ou 5.
+#Regras:
+#Considere apenas números menores que `n`
+#Inclua múltiplos de 3, de 5 ou de ambos
 
