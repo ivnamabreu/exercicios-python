31575bf6c705b88abaa6f1d4e025b6c3701c9037
raianecardoso <raianecardoso.rede@gmail.com>
2026-07-09 15:25:21 -0300

    Ajusta teste do exercício inserir posição.

--- exercicios/02_extra/inserir_posicao/tests/test_.py
+++ exercicios/02_extra/inserir_posicao/tests/test_.py
@@ -1,21 +1,20 @@
-from metro_milimetro.main import resposta
+from inserir_posicao.main import resposta
 import inspect
 import pytest
 
 
 def test_not_none():
-    assert resposta(10) is not None, "Esperado valor diferente de 'None'"
+    assert resposta(['banana', 'manga'], 1, 'laranja') is not None, "Esperado valor diferente de 'None'"
 
 
 def test_type():
-    assert type(resposta(13)) == int or type(resposta(3.2)) == float, "Esperado um inteiro ou float"
+    assert type(resposta(['banana', 'manga'], 1, 'laranja')) == list, "Esperado uma lista"
 
 
 def test_parameters():
-    assert len(inspect.getfullargspec(resposta).args) == 1, "Assinatura da função deverá receber um parâmetro"
+    assert len(inspect.getfullargspec(resposta).args) == 3, "Assinatura da função deverá receber três parâmetros"
 
 
 def test_options_resposta():
-    assert resposta(10) == 10000, f"Esperado valor 10000"
-    assert resposta(1.2) == 1200, f"Esperado valor 1200"
-    assert resposta(0.93) == 930, f"Esperado valor 930"
+    assert resposta(['banana', 'manga'], 1, 'laranja') == ['banana', 'laranja', 'manga'], f"Esperado valor ['banana', 'laranja', 'manga']"
+    assert resposta(['b', 'c'], 0, 'a') == ['a', 'b', 'c'], f"Esperado valor ['a', 'b', 'c']"
