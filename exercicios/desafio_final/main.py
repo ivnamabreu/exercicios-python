4fcd12da6cf6d2568fc4928b6e3e502e62f25061
Ívna Mascarenhas e Abreu <ivna.mabreu@gmail.com>
2026-09-16 20:14:53 +0000

    Add Desafio Final

--- exercicios/desafio_final/main.py
+++ exercicios/desafio_final/main.py
@@ -1,2 +1,24 @@
 def resposta(mensagem, chave):
-    pass
+
+    nova_mensagem = []
+
+    for c in mensagem:
+        if c.isalpha():
+            char = c
+            posicao = ord(char) - ord("A")
+            posicao = (posicao - chave) % 26
+            nova_letra = chr(posicao + ord("A"))
+            nova_mensagem.append(nova_letra)
+        else:
+            nova_mensagem.append(c)
+    
+    return "".join(nova_mensagem)
+
+mensagem = "ZKBKLOXC, UFXK WKCMKBOXRKC O KLBOE! FYMO MYXMVESE Y MEBCY DBSVRK NOF ZIDRYX"
+chave = 10
+mensagem_decodificada = resposta(mensagem, chave)
+print(mensagem_decodificada)
+
+
+
+
